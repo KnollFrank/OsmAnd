01fad5e4bc508d922cc6f4ddecce6631f25a9b18
frankknoll <Knoll_Frank@web.de>
2024-01-17 16:25:37 +0100

    setting bearing

--- OsmAnd/src/net/osmand/plus/simulation/OsmAndLocationSimulation2.java
+++ OsmAnd/src/net/osmand/plus/simulation/OsmAndLocationSimulation2.java
@@ -22,6 +22,7 @@ import org.labyrinth.footpath.core.StepDetection;
 import org.labyrinth.footpath.graph.Node;
 import org.labyrinth.footpath.graph.NodeBuilder;
 import org.labyrinth.footpath.graph.PathFactory;
+import org.labyrinth.footpath.graph.PathPosition;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,16 +43,20 @@ public class OsmAndLocationSimulation2 extends OsmAndLocationSimulation {
                         stepDirection -> {
                             LOG.info("step detected: " + stepDirection);
                             this.navigator.stepInDirection(stepDirection);
-                            app
-                                    .getLocationProvider()
-                                    .setLocationFromSimulation(
-                                            asLocation(
-                                                    this.navigator.getCurrentPathPosition().getGeodetic()));
+                            app.getLocationProvider().setLocationFromSimulation(
+                                    asLocation(this.navigator.getCurrentPathPosition()));
                         });
     }
 
+    private static Location asLocation(final PathPosition currentPathPosition) {
+        final Location location = asLocation(currentPathPosition.getGeodetic());
+        location.setBearing((float) currentPathPosition.asEdgePosition().edge.getDirection().toDegrees());
+        return location;
+    }
+
     private static Location asLocation(final Geodetic geodetic) {
-        return new Location("",
+        return new Location(
+                "",
                 geodetic.latitude.toRadians(),
                 geodetic.longitude.toRadians());
     }
@@ -67,7 +72,7 @@ public class OsmAndLocationSimulation2 extends OsmAndLocationSimulation {
                 new Navigator(
                         PathFactory.createPath(asNodes(directions)),
                         // StepLengthProvider.getStepLength(getQuantity(272.0, CENTI(METRE)))
-                        getQuantity(2.0, METRE));
+                        getQuantity(15.0, METRE));
         notifyListeners(true);
         this.stepDetection.load();
     }
