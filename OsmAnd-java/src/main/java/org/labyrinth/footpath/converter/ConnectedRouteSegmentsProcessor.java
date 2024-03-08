e3a45fb6c7b46d570b4b31358e4d49aef3a4494e
Frank Knoll <Knoll_Frank@web.de>
2024-03-08 10:51:04 +0100

    refactoring

--- OsmAnd-java/src/main/java/org/labyrinth/footpath/converter/ConnectedRouteSegmentsProcessor.java
+++ OsmAnd-java/src/main/java/org/labyrinth/footpath/converter/ConnectedRouteSegmentsProcessor.java
@@ -1,18 +1,13 @@
 package org.labyrinth.footpath.converter;
 
-import static org.labyrinth.common.SetUtils.union;
-
 import com.google.common.collect.ImmutableSet;
 
 import net.osmand.router.postman.RouteSegmentWithEquality;
 
-import org.jgrapht.alg.util.Pair;
-import org.labyrinth.common.ListUtils;
-
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 class ConnectedRouteSegmentsProcessor<T> {
 
@@ -26,31 +21,22 @@ class ConnectedRouteSegmentsProcessor<T> {
     }
 
     public final T processConnectedRouteSegments(final RouteSegmentWithEquality start) {
-        Set<RouteSegmentWithEquality> routeSegments = Collections.singleton(start);
-        T t;
-        boolean newRouteSegmentsFound;
-        do {
-            final Pair<T, Set<RouteSegmentWithEquality>> newTAndRouteSegments = getTAndRouteSegments(routeSegments);
-            final Set<RouteSegmentWithEquality> newRouteSegments = newTAndRouteSegments.getSecond();
-            t = newTAndRouteSegments.getFirst();
-            newRouteSegmentsFound = !newRouteSegments.equals(routeSegments);
-            routeSegments = newRouteSegments;
-        } while (newRouteSegmentsFound);
-        return t;
+        final Set<RouteSegmentWithEquality> routeSegments2Process = new HashSet<>();
+        routeSegments2Process.add(start);
+        final Set<RouteSegmentWithEquality> routeSegmentsAlreadyProcessed = new HashSet<>();
+        final List<T> ts = new ArrayList<>();
+        while (!routeSegments2Process.isEmpty()) {
+            final RouteSegmentWithEquality routeSegment2Process = routeSegments2Process.stream().findAny().get();
+            if (!routeSegmentsAlreadyProcessed.contains(routeSegment2Process)) {
+                ts.add(getT(routeSegment2Process, routeSegments2Process));
+                routeSegmentsAlreadyProcessed.add(routeSegment2Process);
+            }
+            routeSegments2Process.removeAll(routeSegmentsAlreadyProcessed);
+        }
+        return connectedRouteSegmentsVisitor.combine(ts);
     }
 
-    private Pair<T, Set<RouteSegmentWithEquality>> getTAndRouteSegments(final Set<RouteSegmentWithEquality> routeSegments) {
-        final List<Pair<T, Set<RouteSegmentWithEquality>>> tAndRouteSegmentsList =
-                routeSegments
-                        .stream()
-                        .map(this::getTAndRouteSegments)
-                        .collect(Collectors.toList());
-        return Pair.of(
-                getT(tAndRouteSegmentsList),
-                getRouteSegments(tAndRouteSegmentsList));
-    }
-
-    private Pair<T, Set<RouteSegmentWithEquality>> getTAndRouteSegments(final RouteSegmentWithEquality start) {
+    private T getT(final RouteSegmentWithEquality start, final Set<RouteSegmentWithEquality> routeSegments2Process) {
         final Set<RouteSegmentWithEquality> routeSegmentsStartingAtEndOfStart = connectedRouteSegmentsProvider.getRouteSegmentsStartingAtEndOf(start);
         final Set<RouteSegmentWithEquality> routeSegmentsStartingAtStartOfStart = connectedRouteSegmentsProvider.getRouteSegmentsStartingAtStartOf(start);
         final T t =
@@ -64,14 +50,7 @@ class ConnectedRouteSegmentsProcessor<T> {
                         .addAll(routeSegmentsStartingAtEndOfStart)
                         .addAll(routeSegmentsStartingAtStartOfStart)
                         .build();
-        return Pair.of(t, routeSegments);
-    }
-
-    private T getT(final List<Pair<T, Set<RouteSegmentWithEquality>>> tAndRouteSegmentsList) {
-        return connectedRouteSegmentsVisitor.combine(ListUtils.getFirsts(tAndRouteSegmentsList));
-    }
-
-    private Set<RouteSegmentWithEquality> getRouteSegments(final List<Pair<T, Set<RouteSegmentWithEquality>>> tAndRouteSegmentsList) {
-        return union(ListUtils.getSeconds(tAndRouteSegmentsList));
+        routeSegments2Process.addAll(routeSegments);
+        return t;
     }
 }
